ef4e5ce2867802ceecc9d0e09a75a35648841e22
Oleg Alekseenko <aos@lib.unn.ru>
2015-11-24 16:01:59 +0300

    Test site for C# 6.0 exception filters strange behaivior

---
+++ ExceptionFilterWrongBehaivior/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace ExceptionFilterWrongBehaivior
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            int i = 3;
+            do
+            {
+                try
+                {
+                    throw new Exception();
+                }
+                catch (Exception) when (--i < 0)
+                {
+                    throw;
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(10);
+                }
+            } while (true);
+        }
+    }
+}
